02fdd8bea888e10c85c225af17f235e77dcc9d17
Ruben Saiz Molero <ruben.saiz@babelgroup.com>
2022-05-10 14:17:44 +0200

    Ejercicio viajes

--- Rest/13_MicroservicioReservasAgencia/src/main/java/init/SecurityConfig.java
+++ Rest/13_MicroservicioReservasAgencia/src/main/java/init/SecurityConfig.java
@@ -1,18 +1,31 @@
 package init;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
+		/*auth
         .inMemoryAuthentication()
         .withUser("user1")
           .password("{noop}user1") //lo de {noop} se pone para no obligar a usar mecanismo de encriptación
@@ -25,6 +38,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .withUser("user2")
           .password("{noop}user2")
           .roles("OPERATOR");
+          */
+		
+		auth.jdbcAuthentication()
+		  .passwordEncoder(new BCryptPasswordEncoder())
+		  .dataSource(dataSource())
+      	.usersByUsernameQuery("select user, pwd, enabled"
+          	+ " from users where user=?")
+      	.authoritiesByUsernameQuery("select user, rol "
+          	+ "from roles where user=?");
+
 
 	}
 
@@ -32,10 +55,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 		.authorizeRequests()
-		.antMatchers(HttpMethod.GET,"reservas").hasRole("ADMIN")
+		.antMatchers(HttpMethod.GET,"reservas").authenticated()
 		.and()
 		.httpBasic();
 	}
+	
+	private DataSource dataSource() {
+		DriverManagerDataSource ds=new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/springsecurity?serverTimezone=UTC");
+		ds.setUsername("root");
+		ds.setPassword("");
+		return ds;
+	}
+
 
 }
 
